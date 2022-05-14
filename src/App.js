a5971838aa30c217f173611de598408be98d0be3
Nasim Reza <nasim8382@gmail.com>
2022-05-14 09:36:44 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
-      <button class="btn btn-primary">Button</button>
-      <h1 className='text-5xl my-5'>This is doctors-portal</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit similique pariatur tempora quam mollitia maxime nobis ipsum natus doloremque itaque! Aut, molestiae adipisci, quos id inventore ipsum maiores eius consequatur culpa animi unde, quisquam exercitationem architecto consequuntur odit sit incidunt tenetur voluptates. Necessitatibus, doloremque quam labore suscipit in laboriosam dignissimos.</p> <br/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit similique pariatur tempora quam mollitia maxime nobis ipsum natus doloremque itaque! Aut, molestiae adipisci, quos id inventore ipsum maiores eius consequatur culpa animi unde, quisquam exercitationem architecto consequuntur odit sit incidunt tenetur voluptates. Necessitatibus, doloremque quam labore suscipit in laboriosam dignissimos.</p> <br/>
-      <h2 className='text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt harum fugit quasi veritatis voluptas a! Laborum, sapiente obcaecati eius tempore dolorem numquam ducimus assumenda fugit placeat accusamus est itaque maxime cum quisquam eos ea quia a laboriosam illo animi similique odit et autem explicabo? Veniam voluptatibus laborum atque reiciendis ducimus consequuntur corrupti. Possimus est saepe itaque nihil hic, placeat dolores necessitatibus sequi veniam omnis facilis aliquid alias dolorum dicta ipsam molestiae quos earum sapiente in quam quibusdam illum inventore. Perferendis officia quidem eveniet, reprehenderit dolorem tenetur explicabo labore numquam amet, dolorum aspernatur quam reiciendis nesciunt, delectus modi nulla. Eligendi, cumque.</h2>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
